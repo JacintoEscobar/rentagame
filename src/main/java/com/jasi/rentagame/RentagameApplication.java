ff414b4439d8530eb4cf192ab9a92448c9c1cfe6
Jacinto Escobar <eqjo190657@upemor.edu.mx>
2024-05-10 08:26:45 -0700

    initial commit

---
+++ src/main/java/com/jasi/rentagame/RentagameApplication.java
@@ -0,0 +1,13 @@
+package com.jasi.rentagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentagameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentagameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jasi/rentagame/RentagameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasi.rentagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentagameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
